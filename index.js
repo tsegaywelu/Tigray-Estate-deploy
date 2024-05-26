460058be57a7ae08b13108d648aa17cddb9cfbe2
tsegay welu <tsegaygebrezgi56@gmail.com>
2024-05-26 11:42:49 +0300

    i add the build tools

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path"; //i am importing here to add the dist folder
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +25,15 @@ import listrouter from "./routes/listingrouter.js";
 app.use("/api/auth", authrouter);
 app.use("/api/user", userrouter);
 app.use("/api/listing", listrouter);
+//adding the dist folder created after build my code
+//and it shuold be after the api routes unless it will not work
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+//below for any request it will run the index.html.
+// This is particularly useful in Single Page Applications (SPAs)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
